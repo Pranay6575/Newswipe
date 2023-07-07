84dec5ed30235acc0f7f5f986fd01c97057342d7
Pranay <pranaypatil6575@gmail.com>
2023-07-07 09:46:16 +0530

    NewsItem and News Component update by props and state

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-   let  {title, description} = this.props;
+   let  {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
         <div className="card" style={{width: '18rem'}}>
-          <img src="https://cdn.24.co.za/files/Cms/General/d/8744/a7f124255a864201a3c33169cf45ab80.jpg" className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
                  <p className="card-text">{description}</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                  <a href="#" className="btn btn-sm btn-primary">Read More</a>
      </div>
  </div>
       </div>
